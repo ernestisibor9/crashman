0ca8fecd68c2d31b08e1b6e7928b42cf2ba4e09c
Ernest <ernestisibor9@gmail.com>
2023-09-15 10:45:52 +0100

    Change archor

--- index2.php
+++ index2.php
@@ -13,5 +13,6 @@
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolore amet consequuntur laborum magnam porro sit,
     quaerat dignissimos sint rem illum hic, voluptatum ducimus aspernatur iste. Assumenda totam praesentium et!
   </p>
+  <a href="#">Read more ...</a>
 </body>
 </html>
