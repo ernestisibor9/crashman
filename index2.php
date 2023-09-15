f22bc0dd7cf489e6ad5f56597f8591edad0e0b49
Ernest <ernestisibor9@gmail.com>
2023-09-15 10:39:53 +0100

    New commit

--- index2.php
+++ index2.php
@@ -4,9 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <h1>Hello world</h1>
   <a href="">Click me</a>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolore amet consequuntur laborum magnam porro sit,
+    quaerat dignissimos sint rem illum hic, voluptatum ducimus aspernatur iste. Assumenda totam praesentium et!
+  </p>
 </body>
 </html>
